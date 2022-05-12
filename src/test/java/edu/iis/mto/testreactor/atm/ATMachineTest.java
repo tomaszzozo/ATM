bd17dc95ed17341e0d74c55a0c7ff419b4389295
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-05-13 00:54:50 +0200

    Parametry null konstruktora

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -1,19 +1,35 @@
 package edu.iis.mto.testreactor.atm;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.hamcrest.Matchers;
+import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class ATMachineTest {
 
+    @Mock Bank bank;
+
+    private ATMachine atm;
+    private final Currency irrelevantCurrency = Currency.getInstance("PLN");
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() {
+        atm = new ATMachine(bank, irrelevantCurrency);
+    }
 
     @Test
-    void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
+    void constructorNullParameters() {
+        assertThrows(NullPointerException.class, () -> new ATMachine(null, irrelevantCurrency));
+        assertThrows(NullPointerException.class, () -> new ATMachine(bank, null));
     }
 
 }
