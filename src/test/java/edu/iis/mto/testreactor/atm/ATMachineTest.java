c9f7572d7453a7de642d6aef816ba769d242e972
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-05-13 11:55:33 +0200

    Test pobrania depozytu

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -8,10 +8,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Currency;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ATMachineTest {
@@ -19,17 +20,33 @@ class ATMachineTest {
     @Mock Bank bank;
 
     private ATMachine atm;
-    private final Currency irrelevantCurrency = Currency.getInstance("PLN");
+    private final Currency standardCurrency = Currency.getInstance("PLN");
 
     @BeforeEach
     void setUp() {
-        atm = new ATMachine(bank, irrelevantCurrency);
+        atm = new ATMachine(bank, standardCurrency);
     }
 
     @Test
     void constructorNullParameters() {
-        assertThrows(NullPointerException.class, () -> new ATMachine(null, irrelevantCurrency));
+        assertThrows(NullPointerException.class, () -> new ATMachine(null, standardCurrency));
         assertThrows(NullPointerException.class, () -> new ATMachine(bank, null));
+    }
+
+    @Test
+    void setDepositNullParameter() {
+        assertThrows(NullPointerException.class, () -> atm.setDeposit(null));
+    }
+
+    @Test
+    void getCurrentDepositTest() {
+        ArrayList<BanknotesPack> banknotes = new ArrayList<>(Collections.singletonList(BanknotesPack.create(10, Banknote.PL_10)));
+        MoneyDeposit deposit = MoneyDeposit.create(standardCurrency, banknotes);
+        atm.setDeposit(deposit);
+
+        MoneyDeposit result = atm.getCurrentDeposit();
+        assertEquals(standardCurrency, result.getCurrency());
+        assertEquals(banknotes.get(0), result.getBanknotes().get(0));
     }
 
 }
