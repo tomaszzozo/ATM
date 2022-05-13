e19fe71bb96a2926e4cbf3ecbf368e7df458d20b
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-05-13 15:27:33 +0200

    Test wypłaty pieniędzy w niedostępnej walucie

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -17,23 +17,26 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class ATMachineTest {
 
-    @Mock Bank bank;
+    @Mock Bank bankMock;
 
     private ATMachine atm;
     private final Currency standardCurrency = Currency.getInstance("PLN");
-    private final Card irrelevantCard = Card.create("1234123412341234");
-    private final Money irrelevantMoney = new Money(10, standardCurrency);
-    private final PinCode irrelevantPin = PinCode.createPIN(1, 2, 3, 4);
+    private final Card standardCard = Card.create("1234123412341234");
+    private final Money standardMoney = new Money(10, standardCurrency);
+    private final PinCode standardPin = PinCode.createPIN(1, 2, 3, 4);
+    private final ArrayList<BanknotesPack> standardBanknotes =
+            new ArrayList<>(Collections.singletonList(BanknotesPack.create(10, Banknote.PL_10)));
+    private final MoneyDeposit standardDeposit = MoneyDeposit.create(standardCurrency, standardBanknotes);
 
     @BeforeEach
     void setUp() {
-        atm = new ATMachine(bank, standardCurrency);
+        atm = new ATMachine(bankMock, standardCurrency);
     }
 
     @Test
     void constructorNullParameters() {
         assertThrows(NullPointerException.class, () -> new ATMachine(null, standardCurrency));
-        assertThrows(NullPointerException.class, () -> new ATMachine(bank, null));
+        assertThrows(NullPointerException.class, () -> new ATMachine(bankMock, null));
     }
 
     @Test
@@ -43,20 +46,28 @@ class ATMachineTest {
 
     @Test
     void getCurrentDepositTest() {
-        ArrayList<BanknotesPack> banknotes = new ArrayList<>(Collections.singletonList(BanknotesPack.create(10, Banknote.PL_10)));
-        MoneyDeposit deposit = MoneyDeposit.create(standardCurrency, banknotes);
-        atm.setDeposit(deposit);
+
+        atm.setDeposit(standardDeposit);
 
         MoneyDeposit result = atm.getCurrentDeposit();
         assertEquals(standardCurrency, result.getCurrency());
-        assertEquals(banknotes.get(0), result.getBanknotes().get(0));
+        assertEquals(standardBanknotes.get(0), result.getBanknotes().get(0));
     }
 
     @Test
     void withdrawNullParameters() {
-        assertThrows(NullPointerException.class, () -> atm.withdraw(null, irrelevantCard, irrelevantMoney));
-        assertThrows(NullPointerException.class, () -> atm.withdraw(irrelevantPin, null, irrelevantMoney));
-        assertThrows(NullPointerException.class, () -> atm.withdraw(irrelevantPin, irrelevantCard, null));
+        assertThrows(NullPointerException.class, () -> atm.withdraw(null, standardCard, standardMoney));
+        assertThrows(NullPointerException.class, () -> atm.withdraw(standardPin, null, standardMoney));
+        assertThrows(NullPointerException.class, () -> atm.withdraw(standardPin, standardCard, null));
+    }
+
+    @Test
+    void withdrawUnavailableCurrency() {
+        Currency unavailableCurrency = Currency.getInstance("GBP");
+        Money moneyInWrongCurrency = new Money(1, unavailableCurrency);
+
+        ATMOperationException result = assertThrows(ATMOperationException.class, () -> atm.withdraw(standardPin, standardCard, moneyInWrongCurrency));
+        assertEquals(ErrorCode.WRONG_CURRENCY, result.getErrorCode());
     }
 
 }
